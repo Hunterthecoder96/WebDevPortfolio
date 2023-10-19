825e805ceee03128b9322afc1aad370427e556e2
John Hunter Skolnick <jhskolnick100@gmail.com>
2023-10-19 06:56:27 -0400

    Updated about section

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -8,9 +8,17 @@ export default function About(){
         <div className="about-container">
             <div className="backing">
                 <h2>About</h2>
-                <p>As a Full Stack Web Developer I possess the skills and knowledge to create visually appealing web applications with full functionality for the customer and the user.  With my background in marketing I have the skills to make a creative and aesthetically pleasing UX/UI interface.   I have made the shift into the tech field and have not looked back.  I have created applications which have real world applications or mimic real applications. </p>
-               <br/>
-                <p>I have completed V-School's Mastery full-stack web development program and havn't looked back.  At this program I worked alongside profesionals who focus on mastery of each language.</p>
+                <p>As a Full Stack Web Developer, I bring a unique blend of technical expertise and a creative edge from my background in marketing. 
+                    I specialize in crafting visually stunning and fully functional web applications, ensuring both the customer's and user's needs are met.</p>
+                    <br/>
+                <p>My journey into the tech industry has been a transformative one, and I haven't looked back. 
+                    I pride myself on creating applications that not only possess real-world applications but also emulate the user experience of established applications. 
+                    My dedication to excellence led me to successfully complete V-School's rigorous Mastery full-stack web development program, where I had the privilege of collaborating with industry professionals who emphasized the mastery of each programming language.</p>
+                    <br/>
+                <p>My comprehensive skill set allows me to seamlessly blend the worlds of web development and marketing, resulting in web applications that are not only technically sound but also aesthetically pleasing and user-friendly.
+                     This holistic approach enables me to create digital solutions that not only meet technical requirements but also resonate with end users on a deeper level.</p>
+                     <br/>
+                <p>I'm passionate about staying at the forefront of industry trends and emerging technologies, ensuring that I continue to deliver outstanding results in the ever-evolving tech landscape. I'm excited to contribute my expertise to the dynamic tech field, and I look forward to new opportunities for innovation and growth.</p>
             </div>
         </div>
     )
