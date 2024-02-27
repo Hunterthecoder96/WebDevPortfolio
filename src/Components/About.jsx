4cdce1f6fe115537fb19a824e7451794196d35c9
John Hunter Skolnick <jhskolnick100@gmail.com>
2024-02-27 11:57:52 -0500

    photography

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -36,7 +36,7 @@ export default function About() {
     <p>My journey into the tech industry has been a transformative one, and I haven't looked back. 
      I pride myself on creating applications that not only possess real-world applications but also emulate the user experience of established applications. 
     My dedication to excellence led me to successfully complete V-School's rigorous Mastery full-stack web development program, where I had the privilege of collaborating with industry professionals who emphasized the mastery of each programming language.</p>                 <br/>
- <p>My comprehensive skill set allows me to seamlessly blend the worlds of web development and marketing, resulting in web applications that are not only technically sound but also aesthetically pleasing and user-friendly.
+    <p>My comprehensive skill set allows me to seamlessly blend the worlds of web development and marketing, resulting in web applications that are not only technically sound but also aesthetically pleasing and user-friendly.
                       This holistic approach enables me to create digital solutions that not only meet technical requirements but also resonate with end users on a deeper level.</p>
         <br/>
     <p>I'm passionate about staying at the forefront of industry trends and emerging technologies, ensuring that I continue to deliver outstanding results in the ever-evolving tech landscape. I'm excited to contribute my expertise to the dynamic tech field, and I look forward to new opportunities for innovation and growth.</p>           
--- src/Components/Photography.jsx
+++ src/Components/Photography.jsx
@@ -1,23 +1,40 @@
 import React from "react";
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 import Bucks from "../assets/lovebuck.png"
 import BowSeason from "../assets/pat bow season.png"
 import Avon from "../assets/kinakeet gold.png"
 import"../Styles/Photography.css"
 import Cam from "../assets/wired-outline-61-camera.gif"
+import Empty from"../assets/Empty perfection.png"
 export default function Photography(){
+    const settings = {
+        dots: true,
+        infinite: true,
+        speed: 500,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        autoplay:true,
+        autoplayspeed:3000,
+    };
+
 
     return(
         <div className="photo-container">
             <div className="photo-head">
                 <h1>What I enjoy in my freetime</h1>
-            <img className="camera" src={Cam}/>
+            <img className="camera" src={Cam} alt="Camera"/>
             </div>
             
-            <div className="images">
-            <img src={Bucks}/>
-            <img src={BowSeason}/>
-            <img src={Avon}/>
-            </div>
+            <Slider {...settings}>
+            
+            <img className="images" src={Bucks}/>
+            <img className="images" src={Empty}/>
+            <img className="images" src={BowSeason}/>
+            <img className="images" src={Avon}/>
+            </Slider>
+    
         </div>
     )
 }
